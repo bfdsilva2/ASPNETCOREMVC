a3c47ba7d320f497e98b5551bcce7bdc4ba30a36
bfdsi <bfdsi@LAPTOP-56B8E63F>
2023-02-20 15:45:09 -0300

    Add project files.

---
+++ ASPNETCOREMVC/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ASPNETCOREMVC.Models;
+
+namespace ASPNETCOREMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpPost]
+        [HttpGet]
+        public IActionResult Index(HomeModel formulario)
+        {
+            CarregarListaDados();
+            return View();
+        }
+
+        public void CarregarListaDados()
+        {
+            HomeModel objHomeModel = new HomeModel();
+            ViewBag.Lista = objHomeModel.GetAll();
+        }
+    }
+}
---
+++ ASPNETCOREMVC/Models/HomeModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCOREMVC.Models
+{
+    public class HomeModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public List<HomeModel> GetAll()
+        {
+            HomeModel item;
+            List<HomeModel> lista = new List<HomeModel>();
+
+            item = new HomeModel();
+            item.Id = 1;
+            item.Nome = "Filipe";
+            lista.Add(item);
+
+            item = new HomeModel();
+            item.Id = 2;
+            item.Nome = "Aluno";
+            lista.Add(item);
+
+            return lista;
+        }
+    }
+}
